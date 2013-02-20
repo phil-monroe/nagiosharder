6bd2e0f5c452bfa8e3594dc119d83453b4a3d9ed
Phil Monroe <phil@philmonroe.com>
2013-02-20 15:14:05 -0800

    cleaning up

--- lib/nagiosharder/availability.rb
+++ lib/nagiosharder/availability.rb
@@ -1,15 +1,25 @@
 class NagiosHarder
   module Availability
 
-    def host_availability(host, type = :up)
+    def host_availability(host, type = :up, stat = :pct_total)
       host_avail_url = "#{avail_url}?host=#{host}&timeperiod=last24hours&csvoutput=true"
       response =  get(host_avail_url)
 
       raise "wtf #{host_avail_url}? #{response.code}" unless response.code == 200
 
       avail_hash = parse_avail_html(response)
-      avail_hash[type]
+
+      return avail_hash       if type == :all
+      return avail_hash[type] if stat == :all
+      avail_hash[type][stat]
     end
+
+
+    def service_availability(host, service, stat = :ok)
+      service_hash = host_availability(host, :all)[:services][service]
+      stat == :all ? service_hash :  service_hash[stat]
+    end
+
 
 
     def avail_url
@@ -43,25 +53,25 @@ class NagiosHarder
       service_table = tables.pop
       host_table    = tables.pop
 
-      services = Hash.new.tap do |services|
+      services = Hashie::Mash.new.tap do |services|
         rows = service_table.css('tr').map(&:children)[1..-1]
         rows.each do |row|
           r = row.to_a.collect!(&:children).reverse.flatten!
-          services[name_from_element(r.pop)]= {
+          services[name_from_element(r.pop)] = Hashie::Mash.new({
             ok:           pct_from_element(r.pop),
             warning:      pct_from_element(r.pop),
             unknown:      pct_from_element(r.pop),
             critical:     pct_from_element(r.pop),
             undetermined: pct_from_element(r.pop)
-          }
+            })
         end
       end
 
-      Hash.new.tap do |h|
+      Hashie::Mash.new.tap do |h|
         rows = host_table.css('tr').map(&:children)[1..-2]
-        up_row      = rows.slice!(0..2).last.children.to_a
-        down_row    = rows.slice!(0..2).last.children.to_a
-        unreach_row = rows.slice!(0..2).last.children.to_a
+        up_row      = rows[2].children.to_a
+        down_row    = rows[5].children.to_a
+        unreach_row = rows[8].children.to_a
 
         h[:up]           = avail_hash_from_row(up_row)
         h[:down]         = avail_hash_from_row(down_row)
